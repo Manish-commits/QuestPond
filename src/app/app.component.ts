b1b0549cd3d3a23dfbdae4a1c80fc9d318623587
Manish <m.wire2511@gmail.com>
2022-09-24 14:06:26 +0530

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  serverElements = [{type: 'server', name: 'TestServer', content: 'Just a Test'}];
+  // serverElements = [{type: 'server', name: 'TestServer', content: 'Just a Test'}];
 
-  oddNumbers: number[] = [];
-  evenNumbers: number[] = [];
+  // oddNumbers: number[] = [];
+  // evenNumbers: number[] = [];
   
-  onServerAdded(serverData: {serverName: string, serverContent: string}){
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    })
-  };
+  // onServerAdded(serverData: {serverName: string, serverContent: string}){
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: serverData.serverName,
+  //     content: serverData.serverContent
+  //   })
+  // };
 
-  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}){
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    })
-  }
+  // onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}){
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: blueprintData.serverName,
+  //     content: blueprintData.serverContent
+  //   })
+  // }
 
-  onIntervalFired(firedNumber: number) {
-    if(firedNumber % 2 ===0){
-      this.evenNumbers.push(firedNumber);
-    } else{
-      this.oddNumbers.push(firedNumber);
-    }
+  // onIntervalFired(firedNumber: number) {
+  //   if(firedNumber % 2 ===0){
+  //     this.evenNumbers.push(firedNumber);
+  //   } else{
+  //     this.oddNumbers.push(firedNumber);
+  //   }
+  // }
+
+  loadedFeature = 'recepie';
+
+  onNavigate(feature: string) {
+    console.log(feature);
+    this.loadedFeature = feature;
   }
 }
--- src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
+++ src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Recepie } from '../../recepie.model';
 
 @Component({
   selector: 'app-recepie-item',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecepieItemComponent implements OnInit {
 
+  @Input() recepie!: Recepie; 
   constructor() { }
 
   ngOnInit(): void {
